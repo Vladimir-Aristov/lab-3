e1a06aedb27d5f48c48261badd830059d9df1845
pchelka <avv363.aristov@yandex.ru>
2023-03-06 05:24:36 +0300

    finally I made it, hope the code won't make you cry

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 #include <map>
 #include <set>
 #include <exception>
+#include <fstream>
+#include <utility>
 using namespace std;
 
-template<typename Vertex, typename Weight>
+template<typename Vertex = int, typename Weight = int>
 class Graph
 {
 private:
@@ -25,41 +27,13 @@ public:
         }
     }
 
-    // add an edge to the graph
-    void addEdge(Vertex u, Vertex v, Weight w) {
-        // add the vertices if they are not in the graph
-        if (adjList.find(u) == adjList.end()) {
-            addVertex(u);
-        }
-        if (adjList.find(v) == adjList.end()) {
-            addVertex(v);
-        }
+//    Graph(map<Vertex, list<pair<Vertex, Weight>>> const &vertices) {
+//        for (auto& it : vertices) {
+//            adjList[it.first].push_back(it.second);
+//            vertices.insert(it.first);
+//        }
+//    }
 
-        // add the edge
-        adjList[u].push_back(make_pair(v, w));
-        adjList[v].push_back(make_pair(u, w));
-    }
-
-    // remove a vertex from the graph
-    void removeVertex(Vertex v);
-
-    // remove an edge from the graph
-    void removeEdge(Vertex u, Vertex v);
-
-    // get the weight of an edge
-    Weight getWeight(Vertex u, Vertex v);
-
-    // get the list of adjacent vertices
-    list<Vertex> getAdjacentVertices(Vertex v);
-
-
-
-    Graph(map<Vertex, list<pair<Vertex, Weight>>> const &vertices) {
-        for (auto& it : vertices) {
-            adjList[it.first].push_back(it.second);
-            vertices.insert(it.first);
-        }
-    }
     // copy constructor
     Graph(Graph<Vertex, Weight>& other) {
         for (auto& it = other.adjList.begin(); it != adjList.end(); it++) {
@@ -161,9 +135,12 @@ public:
     size_t degree_in(Vertex key) {
         size_t count = 0;
         try {
-            if (adjList.find(key) != adjList.end()) {
-                count = adjList[key].size();
-            } else {
+            for (auto& it : adjList) {
+                if (it.first != key and it.second.find(key) != it.second.end()) {
+                    count++;
+                }
+            }
+            if (count == 0) {
                 invalid_argument error("vertex hasn't been found");
                 throw error;
             }
@@ -188,57 +165,170 @@ public:
         }
         return count;
     }
+
+    // loop
+    bool loop(Vertex key) {
+        bool result = false;
+        try {
+            for (auto& it : adjList.at(key)) {
+            // throws out_of_range exception if there is no such key
+                if (it.first == key) {
+                    result = true;
+                }
+            }
+        } catch (out_of_range& err) {
+            cout << err.what() << "\n";
+        }
+        return result;
+    }
+
+    // insert vertex
+    auto insert_node(Vertex node) {
+        return vertices.insert(node);
+    }
+
+    // insert or assign vertex
+    auto insert_or_assign_node(Vertex node);
+
+    // insert edge
+    auto insert_edge(Vertex u, Vertex v, Weight w) {
+        bool found = true;
+        try {
+            if (!vertices.count(v) or !vertices.count(u)) {
+                found = false;
+                invalid_argument error("not valid vertex");
+                throw error;
+            }
+        } catch (invalid_argument& err) {
+            cout << err.what() << "\n";
+        }
+        auto answer = make_pair(adjList.find(u), found);
+        adjList[u].push_back(make_pair(v, w));
+        return answer;
+    }
+
+    // insert or assign edge
+    auto insert_or_assign_edge(Vertex v, Vertex u, Weight w) {
+        bool found1 = false;
+        bool found2 = false;
+        try {
+            if (vertices.find(v)) {
+                found1 = true;
+            }
+            if (vertices.find(u)) {
+                found2 = true;
+            }
+            if (!(found1 and found2)) {
+                invalid_argument error("invalid vertex");
+                throw error;
+            }
+            // there is a set a vertices in private field
+            // so inserting both nodes in any situation won't
+            // change anything
+            adjList[v].push_back(u, w);
+        } catch (invalid_argument& err) {
+            cout << err.what() << "\n";
+        }
+        auto pos = adjList[v].find(make_pair(u, w));
+        return pos, found1 and found2;
+    }
+
+    // clear edges
+    void clear_edges() {
+        adjList.clear();
+    }
+
+    // clear edges from key
+    bool erase_edges_go_from(Vertex key) {
+        bool found = true;
+        if (!vertices.count(key)) {
+            found = false;
+        }
+        auto it = adjList.find(key);
+        adjList.erase(it);
+        return found;
+    }
+
+    // clear edges go to(Vertex key)
+    bool erase_edges_go_to(Vertex key) {
+        bool found = true;
+        if (!vertices.count(key)) {
+            found = false;
+        }
+        for (auto& it : adjList) {
+            for (auto& it1 = it.second.begin(); it1 != it.second.end(); it1++) {
+                if (it1->first == key) {
+                    adjList.erase(it1);
+                }
+            }
+        }
+        return found;
+    }
+
+    // erase vertex
+    bool erase_node(Vertex key) {
+        bool found = true;
+        if (!vertices.count(key)) {
+            found = false;
+        }
+        {// making it a local variable
+            auto it = adjList.find(key);
+            adjList.erase(it);
+        }
+        for (auto& it : adjList) {
+            for (auto& it1 = it.second.begin(); it1 != it.second.end(); it1++) {
+                if (it1->first == key) {
+                    adjList.erase(it1);
+                }
+            }
+        }
+        vertices.erase(key);
+        return found;
+    }
+
+    // reading from file
+    bool load_from_file(string path) {
+        ifstream inFile;
+        inFile.open(path);
+        bool correct = true;
+        try {
+            if (!inFile) {
+                invalid_argument error("incorrect path");
+                correct = false;
+                throw error;
+            }
+            Vertex from, to;
+            Weight weight;
+            while (inFile >> from >> to >> weight) {
+                insert_or_assign_edge(from, to, weight);
+            }
+
+            inFile.close();
+        } catch (invalid_argument& err) {
+            cout << err.what() << "\n";
+        }
+        return correct;
+    }
+
+    // saving to file
+    void save_to_file(string path) {
+        ofstream outFile;
+        outFile.open(path);
+        try {
+            if (!outFile) {
+                invalid_argument error("incorrect path");
+                throw error;
+            }
+
+            for (auto& it : adjList) {
+                for (auto& it1 = it.second.begin(); it1 != it.second.end(); it1++) {
+                    outFile << it.first << " " << it1->first << " " << it1->second << "\n";
+                }
+
+            }
+            outFile.close();
+        } catch (invalid_argument& err) {
+            cout << err.what() << "\n";
+        }
+    }
 };
-
-// remove a vertex from the graph
-template<typename Vertex, typename Weight>
-void Graph<Vertex, Weight>::removeVertex(Vertex v)
-{
-    // remove the vertex from the graph
-    adjList.erase(v);
-
-    // remove all edges incident to the vertex
-    for (auto &it : adjList)
-    {
-        auto &edges = it.second;
-        edges.remove_if([&](const pair<Vertex, Weight> &edge) {
-            return edge.first == v;
-        });
-    }
-}
-
-// remove an edge from the graph
-template<typename Vertex, typename Weight>
-void Graph<Vertex, Weight>::removeEdge(Vertex u, Vertex v)
-{
-    // remove the edge
-    adjList[u].remove_if([&](const pair<Vertex, Weight> &edge) {
-        return edge.first == v;
-    });
-}
-
-// get the weight of an edge
-template<typename Vertex, typename Weight>
-Weight Graph<Vertex, Weight>::getWeight(Vertex u, Vertex v)
-{
-    // get the weight of the edge
-    for (auto &edge : adjList[u])
-    {
-        if (edge.first == v)
-            return edge.second;
-    }
-    return Weight();
-}
-
-// get the list of adjacent vertices
-template<typename Vertex, typename Weight>
-list<Vertex> Graph<Vertex, Weight>::getAdjacentVertices(Vertex v)
-{
-    list<Vertex> adjVertices;
-    // get the list of adjacent vertices
-    for (auto &edge : adjList[v])
-    {
-        adjVertices.push_back(edge.first);
-    }
-    return adjVertices;
-}
